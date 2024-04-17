66cd74124b095cb2521adfca3a3a1f172ac0d285
aiden omelia <101536960+aidenomelia@users.noreply.github.com>
2024-04-16 21:44:35 -0400

    Add files via upload
    In this project i used TensorFlow to make a neural network for classifying handwritten digits from the MNIST dataset.

---
+++ neuralnetworkDigits.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models, datasets
+import matplotlib.pyplot as plt
+
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
+    layers.Dense(10)
+])
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
+
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
